d82daf060371be2428865c4dd6e97a5f806073f1
Alan Smith <a.bax.smith@gmail.com>
2016-09-06 11:45:12 -0600

    moves logic into js and handlebars templates

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 A small, example app for using Handlebars templates with Node/Express and Webpack
 
 ### UP & RUNNING
-`npm install`
-`npm start`
-`open http://localhost:8080/`
+```
+npm install
+```
+```
+npm start
+```
+```
+open http://localhost:8080/
+```
+
+
+### WHAT IS HAPPENING??
+The rendering logic lives in `src/index.js`. When the document content is loaded, it makes the AJAX call to get the API data, passes it to the appropriate template, and appends it to the corresponding `div` element.
--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './stylesheets/base.scss';
+import Sidebar from './templates/sidebar.handlebars';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // make function calls here
+  fetchPizzerias();
+});
+
+const fetchPizzerias = () => {
+  $.ajax({
+    type: 'get',
+    dataType: 'json',
+    url: 'https://pizza-test.herokuapp.com/api/v1/pizzerias',
+    success: (data) => {
+      // when we get the data back from the API,
+      // build the template and pass the data along.
+      buildSideBar(data);
+    }
+  })
+};
+
+const buildSideBar = (pizzerias) => {
+  const div = document.createElement('div');
+  div.innerHTML = Sidebar({
+    title: "Hello, Alan!",
+    pizzerias: pizzerias
+  });
+  $('#sidebar').append(div);
+};
